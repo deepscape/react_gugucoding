74431eeac1f110235e8b0f05412e83919949dc8f
deepscape <hdkim.scala@gmail.com>
2025-01-19 22:25:06 +0900

    Converted frontend/mall to a regular directory

---
+++ frontend/mall/src/pages/todo/ReadPage.js
@@ -0,0 +1,38 @@
+import { useCallback } from "react";
+import { useParams, useNavigate, createSearchParams, useSearchParams } from "react-router-dom";
+
+const ReadPage = () => {
+    const {tno} = useParams()
+    const navigate = useNavigate()
+    const [queryParams] = useSearchParams()
+
+    const page = queryParams.get("page") ? parseInt(queryParams.get("page")) : 1
+    const size = queryParams.get("size") ? parseInt(queryParams.get("size")) : 10
+
+    const queryStr = createSearchParams({page, size}).toString()
+
+    const moveToModify = useCallback((tno) => {
+        navigate({ 
+            pathname: '/todo/modify/${tno}',
+            search: queryStr
+        })
+    },[tno, page, size])
+
+    const moveToList = useCallback(() => {
+        navigate({
+            pathname: '/todo/list',
+            search: queryStr
+        })
+    }, [page, size])
+    
+    return (
+        <div className="text-3xl font-extrabold">Todo Read page component {tno}
+            <div>
+                <button onClick={() => moveToModify(tno)}>Test Modify</button>
+                <button onClick={() => moveToList(tno)}>Test List</button>
+            </div>
+        </div>
+    );
+}
+
+export default ReadPage;
