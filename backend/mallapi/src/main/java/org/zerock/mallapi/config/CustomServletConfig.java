75186056fe13e8575d00a532a539d7deb2c75566
deepscape <hdkim.scala@gmail.com>
2025-02-04 22:33:26 +0900

    p.124 Chapter 03 완료

--- backend/mallapi/src/main/java/org/zerock/mallapi/config/CustomServletConfig.java
+++ backend/mallapi/src/main/java/org/zerock/mallapi/config/CustomServletConfig.java
@@ -2,6 +2,7 @@ package org.zerock.mallapi.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.zerock.mallapi.formatter.LocalDateFormatter;
 
@@ -11,5 +12,13 @@ public class CustomServletConfig implements WebMvcConfigurer {
     @Override
     public void addFormatters(FormatterRegistry registry) {
         registry.addFormatter(new LocalDateFormatter());
+    }
+
+    public void addCorsMapping(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD")
+                .maxAge(300)
+                .allowedHeaders("Authorization", "Content-Type", "Cache-Control");
     }
 }
--- backend/mallapi/src/main/java/org/zerock/mallapi/controller/TodoController.java
+++ backend/mallapi/src/main/java/org/zerock/mallapi/controller/TodoController.java
@@ -2,14 +2,13 @@ package org.zerock.mallapi.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.zerock.mallapi.dto.PageRequestDTO;
 import org.zerock.mallapi.dto.PageResponseDTO;
 import org.zerock.mallapi.dto.TodoDTO;
 import org.zerock.mallapi.service.TodoService;
+
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -29,5 +28,34 @@ public class TodoController {
         log.info(pageRequestDTO);
 
         return service.list(pageRequestDTO);
+    }
+
+    @PostMapping("/")
+    public Map<String, Long> register(@RequestBody TodoDTO todoDTO) {
+        log.info("TodoDTO: " + todoDTO);
+
+        Long tno = service.register(todoDTO);
+
+        return Map.of("tno", tno);
+    }
+
+    @PutMapping("/{tno}")
+    public Map<String, String> modify(@PathVariable(name = "tno") Long tno, @RequestBody TodoDTO todoDTO) {
+        todoDTO.setTno(tno);
+
+        log.info("Modify: " + todoDTO);
+
+        service.modify(todoDTO);
+
+        return Map.of("RESULT", "SUCCESS");
+    }
+
+    @DeleteMapping("/{tno}")
+    public Map<String, String> remove(@PathVariable(name = "tno") Long tno) {
+        log.info("Remove: " + tno);
+
+        service.remove(tno);
+
+        return Map.of("RESULT", "SUCCESS");
     }
 }
